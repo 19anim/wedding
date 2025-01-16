bf4d84678e0cffdefdd7e69eb11bc436c88ed2ec
Nguyen Phi Tuan An (BD/WPA-PWS1) <nptatdt@gmail.com>
2025-01-16 15:33:16 +0700

    modify groom page. add introduction temp page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Schedule from "./pages/Schedule";
 import StoriesSliderPage from "./pages/StoriesSliderPage";
 import TopUpButton from "./components/topUpBotton/TopUpButton";
 import Groom from "./pages/Groom";
+import IntroductionTemp from "./pages/IntroductionTemp";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Schedule />
       <StoriesSliderPage />
       <Groom />
+      <IntroductionTemp />
       <TopUpButton />
     </div>
   );
---
+++ src/pages/IntroductionTemp.jsx
@@ -0,0 +1,17 @@
+function IntroductionTemp(props) {
+  return (
+    <div className="snap-start h-[100vh] text-white sm:bg-[#fea36e85] font-montserrat text-xs sm:text-base grid grid-cols-3 grid-rows-4">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div className="row-span-2">7</div>
+      <div className="row-span-2">8</div>
+      <div className="row-span-2">9</div>
+    </div>
+  );
+}
+
+export default IntroductionTemp;
