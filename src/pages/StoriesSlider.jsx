392aa5aa1739a30f862c9b3935412dae6a75c6c8
19.anim <nptatdt@gmail.com>
2025-01-25 15:56:15 +0700

    add height props for story slider

--- src/pages/StoriesSlider.jsx
+++ src/pages/StoriesSlider.jsx
@@ -1,7 +1,7 @@
 import StorySlider from "../components/newStorySlider/StorySlider";
 
-const StoriesSlider = () => {
-  return <StorySlider />;
+const StoriesSlider = ({ currentDeviceHeight }) => {
+  return <StorySlider currentDeviceHeight={currentDeviceHeight} />;
 };
 
 export default StoriesSlider;
